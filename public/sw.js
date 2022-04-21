ab8326bea91ecb881fbbeb996c212bceff5165b2
shaozhengyu <497@qq.com>
2022-04-21 17:54:33 +0800

    commit

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install',e=>{
+    console.log('123123')
+});
+self.addEventListener('fetch',e=>{
+
+    console.log(e,'22')
+})
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="chart" ref="chart1" class="chart"/>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent,onMounted,ref } from 'vue'
+import echart from 'echarts';
+console.log(echart,'123')
+export default defineComponent({
+  setup() {
+    console.log(123)
+      let chart1=ref(null);
+      onMounted(()=>{
+         console.log(echart)
+         console.log(chart1.value)
+      })
+      return {
+         chart1,
+      }
+  },
+})
+</script>
+<style lang="scss" scoped>
+  .about{
+     .chart{
+       display: block;
+       width: 300px;
+       height: 300px;
+       background: red;
+     }
+  }
+</style>
